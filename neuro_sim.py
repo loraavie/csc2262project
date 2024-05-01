46b3eb6a0fa773e03138eda5aa87fec73b801bca
Gayoon Nam <143567206+7T0FJ@users.noreply.github.com>
2024-04-30 21:43:42 -0500

    Update neuro_sim.py to include bonus mode

--- neuro_sim.py
+++ neuro_sim.py
@@ -1,14 +1,13 @@
-#Group 8: Gayoon Nam, Kristen Averett, Cassidy McDonald, Lora Elliott, Lynn Nguyen 
+#Group 8: Gayoon Nam, Kristen Averett, Cassidy McDonald, Lora Elliott, Lynn Nguyen
 
-#Many print lines which were used for debugging are commented out. 
+# Many print lines which were used for debugging are commented out.
 
 import numpy as np
 import matplotlib.pyplot as plt
-import tqdm as tqdm
 import argparse
 import json
 
-#Not Used
+# Not used
 def LIF_model_eulers_method(vm, vr, tm, isyn, cm, t, ts, tr, dt):
     """
     Uses Euler's method
@@ -51,27 +50,47 @@ def alpha_synapse_model(w, g, vrev, vm, t, t0, tausyn):
     :param t: current time of simulation (seconds)
     :param t0: time spike started (seconds)
     :param tausyn: (constant) decay time of alpha synapse (seconds)
-    :return: isyn
+    :return: I_syn
     """
-    print("t",t)
-    print("t0", t0)
-    print(-(t-t0)/tausyn)
-    print("val3", np.exp(-(t-t0)/tausyn))
-    return (w*g*(vrev-vm)*((t-t0)/tausyn)*np.exp(-(t-t0)/tausyn))
+    # print("t",t)
+    # print("t0", t0)
+    # print(-(t-t0)/tausyn)
+    # print("val3", np.exp(-(t-t0)/tausyn))
+    return w*g*(vrev-vm)*((t-t0)/tausyn)*np.exp(-(t-t0)/tausyn)
 
 
-# Experiment 5 Taylor Series approximation of e^x centered at 0
-def bonus(x):
+def e(x):
+    """
+    10th order Taylor series approximation of e^x centered at 0 for Experiment 5
+    :param x: input value
+    :return: approximated value
+    """
     return 1 + x + (x**2)/2 + (x**3)/6 + (x**4)/24 + (x**5)/120 + (x**6)/720 + (x**7)/5040 + (x**8)/40320 + (x**9)/362880 + (x**10)/3628000
 
 
-if __name__ == "__main__":
+def alpha_synapse_model_bonus(w, g, vrev, vm, t, t0, tausyn):
+    """
+    Determines the spike's synaptic current using a 10th order Taylor series
+    approximation of e^x centered at 0 instead of the exponential term in Equation 3
+    :param w: (constant) weight of synapse
+    :param g: (constant) maximum conductance of synapse (siemens)
+    :param vrev: (constant) reverse potential (volts)
+    :param vm: membrane voltage of postsynaptic neuron (volts)
+    :param t: current time of simulation (seconds)
+    :param t0: time spike started (seconds)
+    :param tausyn: (constant) decay time of alpha synapse (seconds)
+    :return: I_syn
+    """
+    return w*g*(vrev-vm)*((t-t0)/tausyn)*e(-(t-t0)/tausyn)
 
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser("Program to model the current of a neuron")
     parser.add_argument('m', type=str, help="The value of m; can either be spike or current")
     parser.add_argument('s', type=float, help="The value of s; amount of time to run the simulation in milliseconds")
     parser.add_argument('--spike_rate', default=50, type=int, help="input spike rate in Hz")
     parser.add_argument('--current', default = 1, type=float, help="input current in nanoamps")
+    parser.add_argument('--bonus', default=0, type=int, help="toggle to 1 to use Taylor series approximation of e^x instead of exponential term")
     # Parse the command line arguments
     args = parser.parse_args()
 
@@ -80,11 +99,17 @@ if __name__ == "__main__":
         print("Invalid mode")
         exit(1)
 
+    bonus = args.bonus
+    if bonus != 0 and bonus != 1:
+        print("Invalid bonus mode toggle")
+        exit(1)
+
     # Constants
     try:
-        f = open('\\Users\\loraa\\Downloads\\CSC2262Project\\config.json', 'r')
+        # Change to correct file path
+        f = open('/Users/gynam/IdeaProjects/CSC 2262 HW 4/config.json', 'r')
         data = json.load(f)
-        print(data)
+        # print(data)
         v_r = data["v_r"]
         v_thr = data["v_thr"]
         v_spike = data["v_spike"]
@@ -102,13 +127,12 @@ if __name__ == "__main__":
     except PermissionError:
         print("Permission denied")
 
-    print(v_r, v_thr, v_spike, v_rev, tao_m, tao_syn, c_m, g_bar, t_r, w, dt)
+    # print(v_r, v_thr, v_spike, v_rev, tao_m, tao_syn, c_m, g_bar, t_r, w, dt)
 
     # Simulation time
     sim_time = args.s/1000
 
-
-    # These will store the time and membrane voltage
+    # These will store the times and membrane voltages
     time = 0
     time_array = [time]
     membrane_voltage = v_r
@@ -118,22 +142,22 @@ if __name__ == "__main__":
 
     # Spike mode
     if mode == "spike":
-       # print("SPIKE")
+        # print("SPIKE")
         firstSpike = True
         spike_rate = args.spike_rate
         spike_times = np.arange(0, sim_time, sim_time/spike_rate)
         time_between_spikes = (spike_times[1]-spike_times[0])*10
-       # print(spike_times)
+        # print(spike_times)
 
-        while time < sim_time:
-
-
+        while time <= sim_time:
             time += dt
             t0 = 0
             #problem here (SOLVED)
-            i_syn = alpha_synapse_model(1, g_bar, v_rev, membrane_voltage, time, ts, tao_syn)
+            i_syn = alpha_synapse_model(w, g_bar, v_rev, membrane_voltage, time, ts, tao_syn)
+            if bonus == 1:
+                i_syn = alpha_synapse_model_bonus(w, g_bar, v_rev, membrane_voltage, time, ts, tao_syn)
             #print("Isyn =", i_syn)
-            #print("val1", dt*((-((membrane_voltage-v_r)/tao_m)) ))
+            #print("val1", dt*((-((membrane_voltage-v_r)/tao_m))))
             #print("val2", i_syn/c_m)
             membrane_voltage += dt*((-((membrane_voltage-v_r)/tao_m) + i_syn/c_m)*heaviside_step_function(time, ts, t_r))
             #print("Step Function", heaviside_step_function(time, ts, t_r))
@@ -141,23 +165,18 @@ if __name__ == "__main__":
             if time-ts-t_r > time_between_spikes:
                 ts = time
                 membrane_voltage = v_spike
-
             if membrane_voltage > v_thr:
                 #print("Membrane Voltage 2", membrane_voltage)
                 membrane_voltage = v_spike
                 membrane_voltage_array.append(membrane_voltage)
                 ts = time + dt
-
                 membrane_voltage = v_r
-
-                print("Current Time: ", time, "Start Time of Last Input Spike: ", t0)
+                # print("Current Time: ", time, "Start Time of Last Input Spike: ", t0)
             else:
                 membrane_voltage_array.append(membrane_voltage)
-
-            print("Time", time)
-            print("TS", ts)
+            # print("Time", time)
+            # print("TS", ts)
             time_array.append(time*1000)
-
 
     # Current mode
     if mode == "current":
@@ -167,22 +186,22 @@ if __name__ == "__main__":
             time += dt
             i_syn = current
             membrane_voltage += (dt*((-((membrane_voltage-v_r)/tao_m) + i_syn/c_m)*heaviside_step_function(time, ts, t_r)))
-            print("Membrane Voltage", membrane_voltage)
+            # print("Membrane Voltage", membrane_voltage)
             if membrane_voltage > v_thr:
                 membrane_voltage = v_r
                 membrane_voltage_array.append(v_spike)
-                #print("Membrane Voltage 2", membrane_voltage)
+                # print("Membrane Voltage 2", membrane_voltage)
                 ts = time
             else:
                 membrane_voltage_array.append(membrane_voltage)
-            #print("Time", time)
-            #print("val1", dt * ((-((membrane_voltage - v_r) / tao_m))))
-            #print("val2", i_syn / c_m)
+            # print("Time", time)
+            # print("val1", dt * ((-((membrane_voltage - v_r) / tao_m))))
+            # print("val2", i_syn / c_m)
             time_array.append(time*1000)
 
-#print(spike_times)
-# Plot it
+# print(spike_times)
 # print("Membrane Voltage Array", membrane_voltage_array)
+# Plot it
 plt.plot(time_array, membrane_voltage_array, color='r')
 plt.title("Group 8 Membrane Potential Track", size=16)
 plt.xlabel("Time (msec)", size=16)
